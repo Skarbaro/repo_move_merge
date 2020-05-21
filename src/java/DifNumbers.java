ee05059bfbd779e4d92d6454afdaaf7d4fe55165
Alex Romaniuk <alaster.rom@gmail.com>
2020-05-21 17:31:07 +0300

    rename 4 file / update 2 line 1 file

--- src/java/DifNumbers.java
+++ src/java/DifNumbers.java
@@ -1,14 +1,15 @@
 public class DifNumbers {
 
    public static void main(String[] args) {
-      int num1 = 5, num2 = 15; 
+      int num1 = 5;
+	  int num2 = 15; 
 	  int dif;
       dif = num1 - num2;
       System.out.println("Sum of these numbers: " + dif);
    }
    
    public static void main(String[] args) {
-      int sum1 = 5;
+      int sum1 = 5; 
 	  int sum2 = 15;
 	  int sum;
       sum = sum1 - sum2;
